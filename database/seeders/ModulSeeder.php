b42d724f26a0cca79f50e506fefd649749d6b1fb
hasyim404 <muhaisim7@gmail.com>
2022-11-15 23:37:14 +0700

    add seeder for course&modul

--- database/seeders/ModulSeeder.php
+++ database/seeders/ModulSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class CourseSeeder extends Seeder
+class ModulSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,14 +16,10 @@ class CourseSeeder extends Seeder
     public function run()
     {
         for ($i=0; $i <= 3; $i++) {
-            DB::table('course')->insert([
-                'nama_course' => uniqid('NamaCourse_'),
-                'deskripsi_course' => uniqid('DeskripsiCourse_'),
-                'foto' => null,
-                'jdl_modul' => uniqid('NamaCourse_'),
-                'deskripsi_modul' => uniqid('DeskripsiModul_'),
-                'file_materi' => null,
-                'video' => null,
+            DB::table('modul')->insert([
+                'jdl_modul' => uniqid('jdlModul_'),
+                'course_id' => rand(1,4),
+                'deskripsi' => uniqid('deskripsi_'),
                 'created_at' => new \DateTime,
                 'updated_at' => null,
             ]);
