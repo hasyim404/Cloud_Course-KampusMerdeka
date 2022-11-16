a61605a57597a24ba81f6526b46b44179d2b73ab
hasyim404 <muhaisim7@gmail.com>
2022-11-17 03:50:20 +0700

    CRUD Users with Upload Image

---
+++ resources/views/admin/users/detail.blade.php
@@ -0,0 +1,89 @@
+@extends('admin.index')
+@section('title', 'Detail User')
+@section('info', 'Detail User')
+@section('data1', 'Kelola User')
+@section('data2', 'Detail User')
+@section('content')
+<section class="section profile">
+    <div class="row">
+      <div class="col-xl-4">
+
+        <div class="card">
+          <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+            @empty($data->foto)
+                <img src="{{ url('img/users_profile/!profile-default.jpg') }}" height="120px" alt="Profile" class="rounded-circle">
+            @else
+                <img src="{{ url('img/users_profile')}}/{{$data->foto}}" height="120px" alt="Profile" class="rounded-circle">
+            @endempty
+            <h2>{{ $data->f_name }} {{ $data->l_name }}</h2>
+            <h3>{{ $data->email }}</h3>
+            <div class="social-links mt-2">
+              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-xl-8">
+
+        <div class="card">
+          <div class="card-body pt-3">
+            <!-- Bordered Tabs -->
+            <ul class="nav nav-tabs nav-tabs-bordered">
+
+              <li class="nav-item">
+                <button class="nav-link active" data-bs-toggle="tab" data-bs-target="#profile-overview">Overview</button>
+              </li>
+
+            </ul>
+            <div class="tab-content pt-2">
+
+              <div class="tab-pane fade show active profile-overview" id="profile-overview">
+        
+                <h5 class="card-title">Profile Details</h5>
+
+                <div class="row">
+                  <div class="col-lg-3 col-md-4 label ">Full Name</div>
+                  <div class="col-lg-9 col-md-8">{{ $data->f_name }} {{ $data->l_name }}</div>
+                </div>
+
+                <div class="row">
+                  <div class="col-lg-3 col-md-4 label">Email</div>
+                  <div class="col-lg-9 col-md-8">{{ $data->email }}</div>
+                </div>
+
+                <div class="row">
+                  <div class="col-lg-3 col-md-4 label">No. Telp</div>
+                  <div class="col-lg-9 col-md-8">{{ $data->no_telp }}</div>
+                </div>
+
+                <div class="row">
+                  <div class="col-lg-3 col-md-4 label">Username</div>
+                  <div class="col-lg-9 col-md-8">{{ $data->username }}</div>
+                </div>
+
+                <div class="row">
+                  <div class="col-lg-3 col-md-4 label">Status</div>
+                  <div class="col-lg-9 col-md-8">{{ $data->status }}</div>
+                </div>
+
+              </div>
+
+                <div class="text-end">
+                    <a class="btn btn-primary btn-md" href=" {{ url('admin/users') }}">
+                        Back
+                    </a>  
+                </div>
+              
+            </div><!-- End Bordered Tabs -->
+
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/admin/users/form.blade.php
@@ -0,0 +1,112 @@
+@extends('admin.index')
+@section('title', 'Form Kelola User')
+@section('info', 'Form Tambah User')
+@section('data1', 'Kelola User')
+@section('data2', 'Form Tambah User')
+@section('content')
+<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Tambah User</h5>
+
+      <!-- Floating Labels Form -->
+      <form class="row g-3" method="POST" action="{{ route('users.store') }}" enctype="multipart/form-data">
+        @csrf
+
+        @if ($errors->any())
+          <div class="alert alert-danger">
+            {{-- <strong>Whoops!</strong> Ada Salah saat input data --}}
+            <strong>Error!</strong>
+            {{-- <br><br> --}}
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>                        
+                @endforeach
+            </ul>
+          </div>    
+        @endif
+
+        <div class="col-md-6">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="floatingName" name="f_name" placeholder="">
+            <label for="floatingName">First Name</label>
+          </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingName" name="l_name" placeholder="">
+                <label for="floatingName">Last Name</label>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingName" name="email" placeholder="">
+                <label for="floatingName">Email</label>
+            </div>
+        </div>
+        <div class="col-md-6">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="floatingName" name="no_telp" placeholder="Email">
+            <label for="floatingName">No. Telp</label>
+          </div>
+        </div>
+        <div class="col-md-6">
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingName" name="username" placeholder="">
+                <label for="floatingName">Username</label>
+            </div>
+        </div>
+        
+        <div class="col-md-6">
+            <div class="form-floating">
+                <input type="password" class="form-control" id="floatingName" name="password" placeholder="">
+                <label for="floatingName">Password</label>
+            </div>
+        </div>
+        <div class="col-md-3">
+          <label for="validationDefault04" class="form-label">Status</label>
+          <select class="form-select" id="validationDefault04" name="status" required>
+            <option selected disabled value="">-- Pilih Status --</option>
+            @php
+            $no = 1;  
+            @endphp
+
+            @foreach ($ar_status as $status)
+
+            <option value="{{ $status }}"> {{ $no++ }} - {{ $status }}</option> 
+
+            @endforeach
+          </select>
+        </div>
+        <div class="row g-3">
+            <div class="col-md-4">
+                <div class="form-floating">
+                    <input type="file" class="form-control" id="floatingName" name="foto" placeholder="">
+                    <label for="floatingName">Foto</label>
+                </div>
+            </div>    
+        </div>
+        
+        <div class="col-md-3">
+            <label for="validationDefault04" class="form-label">Role</label>
+            <select class="form-select" id="validationDefault04" name="role" required>
+              <option selected disabled value="">-- Pilih Role --</option>
+  
+              @foreach ($ar_role as $role)
+  
+              <option value="{{ $role }}"> {{ $role }}</option> 
+  
+              @endforeach
+            </select>
+        </div>
+        <div class="text-center p-3">
+          <button type="submit" class="btn btn-primary">Tambah</button>
+          <a class="btn btn-secondary btn-md" href=" {{ url('admin/users') }}">
+            Back
+          </a>  
+        </div>
+      </form><!-- End floating Labels Form -->
+
+    </div>
+  </div>
+@endsection
+
